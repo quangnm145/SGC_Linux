7f86ce5c9044e546954876b865d1341c5c14c3ba
QuangNM41 <smalldemon.hsgs@gmail.com>
2024-12-10 16:19:43 +0700

    Linux: Day3 init

--- Linux_Day3/Readme.md
+++ Linux_Day3/Readme.md
@@ -2,4 +2,9 @@
 
 ## File with holes (Sparse files) #
 
-cd
+- Regular file
+
+Khi một file được khởi tạo, một tên tham chiếu đến 1 inode sẽ được khởi tạo. Inode này lưu trữ các thuộc tính: File size, permissions, time, File ownership, type, .... 
+Có thể sử dụng các lệnh ls hoặc stat để kiểm tra những thông tin này
+
+Inode giữ con trỏ  đến các khối dữ liệu trong hệ thống (các dữ liệu được lưu trữ theo từng block)
--- Linux_Day3/ex1.c
+++ Linux_Day3/ex1.c
@@ -1,90 +1,54 @@
+#include <sys/stat.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <string.h>
-#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+// #include "tlpi_hdr.h"
 
-#define BUFFER_SIZE 4096
+#ifndef BUF_SIZE /* Allow "cc -D" to override definition */
+#define BUF_SIZE 1024
+#endif
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[])
+{
+    int input_fd, output_fd;
+    ssize_t numRead;
+    char buf[BUF_SIZE], c;
+ 
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <source file> <destination file>\n", argv[0]);
+        printf("Error argument: %s <source_file> <dest_file>\n", "MyCp");
         return 1;
     }
 
-    // Open source file for reading
-    int src_fd = open(argv[1], O_RDONLY);
-    if (src_fd == -1) {
-        perror("Error opening source file");
+    input_fd = open(argv[1], O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (input_fd == -1) {
+        printf("opening file %s %d", argv[1], 1);
         return 1;
     }
 
-    // Open or create destination file for writing
-    int dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dest_fd == -1) {
-        perror("Error opening destination file");
-        close(src_fd);
-        return 1;
-    }
-
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
-    ssize_t bytes_written;
-    off_t current_offset = 0;
-
-    while ((bytes_read = read(src_fd, buffer, BUFFER_SIZE)) > 0) {
-        size_t hole_start = 0;
-        size_t i;
-
-        for (i = 0; i < bytes_read; ++i) {
-            if (buffer[i] == '\0') {
-                // Start of a hole
-                if (hole_start == (size_t)-1) {
-                    hole_start = i;
-                }
-            } else {
-                // End of a hole, write non-hole data
-                if (hole_start != (size_t)-1) {
-                    // Skip the hole in the destination file
-                    current_offset += i - hole_start;
-                    if (lseek(dest_fd, current_offset, SEEK_SET) == (off_t)-1) {
-                        perror("Error seeking in destination file");
-                        close(src_fd);
-                        close(dest_fd);
-                        return 1;
-                    }
-                    hole_start = (size_t)-1;
-                }
-                // Write the non-null byte
-                if (write(dest_fd, &buffer[i], 1) != 1) {
-                    perror("Error writing to destination file");
-                    close(src_fd);
-                    close(dest_fd);
-                    return 1;
-                }
-                current_offset++;
+    output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (output_fd == -1)
+        printf("opening file %s %d", argv[2], 2);
+ 
+    int holes = 0;
+    while ((numRead = read(input_fd, buf, BUF_SIZE)) > 0) 
+    {
+        for (int i = 0; i < numRead; i++) {
+            if (buf[i] == '\0') {
+                holes++;
+                continue;
+            } else if (holes > 0) {
+                lseek(output_fd, holes, SEEK_CUR);
+                holes = 0;
             }
-        }
-
-        // Handle remaining hole at the end of the buffer
-        if (hole_start != (size_t)-1) {
-            current_offset += bytes_read - hole_start;
-            if (lseek(dest_fd, current_offset, SEEK_SET) == (off_t)-1) {
-                perror("Error seeking in destination file");
-                close(src_fd);
-                close(dest_fd);
+            if (write(output_fd, &buf[i], 1) != 1) {
+                printf("couldnt write char to file");
                 return 1;
             }
         }
     }
-
-    if (bytes_read == -1) {
-        perror("Error reading source file");
-    }
-
-    close(src_fd);
-    close(dest_fd);
-
+ 
+    close(output_fd);
+    close(input_fd);
     return 0;
 }
